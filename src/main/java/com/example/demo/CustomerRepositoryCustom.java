b0ada9aad3a272b523d4524759038ae7dca5582e
anderson1795 <stephenanderson@Stephens-MacBook-Pro.local>
2018-02-11 23:00:05 -0500

    Removing extra files

--- src/main/java/com/example/demo/CustomerRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.query.Procedure;
-
-public interface CustomerRepositoryCustom {
-    @Procedure(name = "GetAllCustomersSP")
-    Object[] getAllCustomersSP();
-}
--- src/main/java/com/example/demo/CustomerSPRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.demo;
-//
-//public class CustomerSPRepository {
-//    private Iterable<CustomerSP> allCustomers;
-//
-//    public Iterable<CustomerSP> getAllCustomers() {
-//        return allCustomers;
-//    }
-//}
